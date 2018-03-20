0c06cabd30defa9de33cbc4e06470e886aca8334
filmote <simon@bloggingadeadhorse.com>
2018-03-20 15:21:28 +1100

    Initial commit,

---
+++ Levels/LodeRunner_Title_loop_revised.c
@@ -0,0 +1,11 @@
+// ArduboyTones stream for file "LodeRunner_Title_loop_revised.mid" created by MIDI2TONES V1.0.0 on Tue Mar 20 15:20:29 2018
+// command line: midi2tones_64bit.exe -o2 LodeRunner_Title_loop_revised 
+//   From channel 0
+const uint16_t score[] PROGMEM = {
+ NOTE_DS3,165, NOTE_REST,168, NOTE_D3,156, NOTE_REST,9, NOTE_AS3,156, NOTE_REST,9, NOTE_E3,156, NOTE_REST,9,
+ NOTE_B3,156, NOTE_REST,9, NOTE_E3,165, NOTE_REST,168, NOTE_AS3,165, NOTE_REST,168, NOTE_C3,156, NOTE_REST,9,
+ NOTE_GS3,156, NOTE_REST,9, NOTE_D3,156, NOTE_REST,9, NOTE_AS3,156, NOTE_REST,9, NOTE_D3,156, NOTE_REST,9,
+ NOTE_CS3,156,
+ TONES_END
+};
+// This score contains 102 bytes.
