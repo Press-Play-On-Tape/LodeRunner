ff74d2c28edb844be317b1af8d7e82025a938a51
filmote <simon@bloggingadeadhorse.com>
2018-08-03 09:54:04 +1000

    Preemptive fix of Level 49. May not be needed.

--- src/levels/Levels.h
+++ src/levels/Levels.h
@@ -643,32 +643,32 @@ const uint8_t PROGMEM level153[] = {
 
 #if GAME_NUMBER == 1
 const uint8_t *levels[] =    { nullptr, 
-                               level1, level2, level3, level4, level5, level6, level7, level8, level9, level10, 
-                               level11, level12, level13, level14, level15, level16, level17, level18, level19, level20, 
-                               level21, level22, level23, level24, level25, level26, level27, level28, level29, level30, 
-                               level31, level32, level33, level34, level35, level36, level37, level38, level39, level40, 
+                               level0, level1, level2, level3, level4, level5, level6, level7, level8, level9, 
+                               level10, level11, level12, level13, level14, level15, level16, level17, level18, level19, 
+                               level20, level21, level22, level23, level24, level25, level26, level27, level28, level29, 
+                               level30, level31, level32, level33, level34, level35, level36, level37, level38, level39, 
                                };
 #endif
 #if GAME_NUMBER == 2
 const uint8_t *levels[] =    { nullptr, 
-                               level41, level42, level43, level44, level45, level46, level47, level48, level49, level50, 
-                               level51, level52, level53, level54, level55, level56, level57, level58, level59, level60, 
-                               level61, level62, level63, level64, level65, level66, level67, level68, level69, level70, 
-                               level71, level72, level73, level74, level75, level76, level77, level78, level79, level80, 
+                               level40, level41, level42, level43, level44, level45, level46, level47, level48, level49, 
+                               level50, level51, level52, level53, level54, level55, level56, level57, level58, level59, 
+                               level60, level61, level62, level63, level64, level65, level66, level67, level68, level69, 
+                               level70, level71, level72, level73, level74, level75, level76, level77, level78, level79, 
                                };
 #endif
 #if GAME_NUMBER == 3
 const uint8_t *levels[] =    { nullptr, 
-                               level81, level82, level83, level84, level85, level86, level87, level88, level89, level90, 
-                               level91, level92, level93, level94, level95, level96, level97, level98, level99, level100, 
-                               level101, level102, level103, level104, level105, level106, level107, level108, level109, level110, 
-                               level111, level112, level113, level114, level115, level116, level117, level118, level119, level120, 
+                               level80, level81, level82, level83, level84, level85, level86, level87, level88, level89, 
+                               level90, level91, level92, level93, level94, level95, level96, level97, level98, level99, 
+                               level100, level101, level102, level103, level104, level105, level106, level107, level108, level109, 
+                               level110, level111, level112, level113, level114, level115, level116, level117, level118, level119, 
                                };
 #endif
 #if GAME_NUMBER == 4
 const uint8_t *levels[] =    { nullptr, 
-                               level121, level122, level123, level124, level125, level126, level127, level128, level129, level130, 
-                               level131, level132, level133, level134, level135, level136, level137, level138, level139, level140, 
-                               level141, level142, level143, level144, level145, level146, level147, level148, level149, level150, 
-                               level151, level152, level153, level154, };
+                               level120, level121, level122, level123, level124, level125, level126, level127, level128, level129, 
+                               level130, level131, level132, level133, level134, level135, level136, level137, level138, level139, 
+                               level140, level141, level142, level143, level144, level145, level146, level147, level148, level149, 
+                               level150, level151, level152, level153, };
 #endif
